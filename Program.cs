41b39b9a739081491b6ad3857bc450ee5cdc8519
Luzielma Silva <129329993+Luzielmalu@users.noreply.github.com>
2023-03-29 19:13:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double numero1;
+double numero2;
+double total;
+string operacao;
+
+
+Console.Write("digite numero1:");
+numero1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("digite a operacao:");
+operacao = (Console.ReadLine());  
+Console.Write("digite numero2:");
+numero2 = Convert.ToDouble(Console.ReadLine());
+
+total = 0;
+
+    if (operacao== "-");{
+    total = numero1 - numero2;
+}
+    
+Console.Write("o resultado é: " + total);
+Console.ReadKey();
+
+
+
